b0cdfb6635b8d7d2b448edf42fec95d721f5e09e
João Augusto <61090559+joaosouzac@users.noreply.github.com>
2025-05-29 17:45:34 -0300

    feat: initial rpc connection

---
+++ RCPTesting/CalculatorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Grpc.Core;
+
+using RPCalculator;
+
+namespace RCPTesting
+{
+    public class CalculatorService : Calculator.CalculatorBase
+    {
+        public override Task<AddReply> Add(AddRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new AddReply { Result = request.X + request.Y });
+        }
+    }
+}
